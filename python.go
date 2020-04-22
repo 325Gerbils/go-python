d556486ae2ff290d57c0698dfb0cf99e63ff56cc
Benjamin Cooley <benjamincooley81@gmail.com>
2020-04-21 18:38:08 -0700

    Create python.go

---
+++ python.go
@@ -0,0 +1,50 @@
+package python
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"os/exec"
+	"time"
+)
+
+var environment string
+
+// Init sets a unique filename for the python environment's state
+func Init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	environment = fmt.Sprintf("%x", rand.Int()) + ".pkl "
+	cmd := exec.Command("python", "-c", `import dill
+filename = '`+environment+`'
+dill.dump_session(filename)`)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(output))
+}
+
+// Destroy deletes the files storing the python environment
+func Destroy() error {
+	err := os.RemoveAll(environment)
+}
+
+// Run executes the given python code in a persistent state interpreter
+func Run(pycode string) (string, error) {
+
+	execCode := `import dill
+
+filename = '` + environment + `'
+dill.load_session(filename)
+
+` + pycode + `
+
+dill.dump_session(filename)`
+
+	cmd := exec.Command("python", "-c", execCode)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+	return string(output), nil
+}
